3f078ba85e920fddbdfeaad8fba4966b83d4c48c
alpha0212 <qkrcjfgml321@naver.com>
2022-11-30 00:57:06 +0900

    Half Complete Intro

--- src/Pages/IntroPage/index.tsx
+++ src/Pages/IntroPage/index.tsx
@@ -4,7 +4,12 @@ import * as S from './styled';
 export const IntroPage: React.FC = () => {
   return (
     <S.IntroContainer>
-      <S.IntroBackground />
+      <S.IntroBackground>
+        <S.MainIntro>어 반가워</S.MainIntro>
+        <S.GoToMain>
+          <div style={{ paddingTop: '3.5rem' }}>숨은그림찾기 하러 가기</div>
+        </S.GoToMain>
+      </S.IntroBackground>
     </S.IntroContainer>
   );
 };
--- src/Pages/IntroPage/styled.ts
+++ src/Pages/IntroPage/styled.ts
@@ -5,18 +5,30 @@ export const IntroContainer = styled.div`
   width: 100%;
   height: 100%;
   position: fixed;
-  background: purple;
+  background: rgba(64, 65, 109);
 `;
 
 export const IntroBackground = styled.div`
   width: 1265px;
-  height: 709px;
+  height: 705px;
+  margin: auto;
   background-image: url('https://cdn.discordapp.com/attachments/606799838721343527/1047169390950490222/image.png');
 `;
 
 export const MainIntro = styled.div`
+  width: 100%;
+  text-align: center;
   font-size: 2.5rem;
   font-family: 'ONE-Mobile-POP';
+  color: white;
+  margin-top: 25rem;
+  animation: MainIntro 3s infinite;
+  @keyframes MainIntro {
+    50% {
+      font-size: 4rem;
+    }
+  }
+
   @font-face {
     font-family: 'ONE-Mobile-POP';
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/ONE-Mobile-POP.woff')
@@ -26,4 +38,19 @@ export const MainIntro = styled.div`
   }
 `;
 
-export const GoToMain = styled.div``;
+export const GoToMain = styled.div`
+  width: 30rem;
+  height: 10rem;
+  text-align: center;
+  font-family: 'ONE-Mobile-POP';
+  color: white;
+  margin: auto;
+  margin-top: 6rem;
+  @font-face {
+    font-family: 'ONE-Mobile-POP';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/ONE-Mobile-POP.woff')
+      format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
